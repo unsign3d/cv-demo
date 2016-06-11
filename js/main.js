21d5739559d3b02e0d8067a6687352ea5f38f71a
Luca Bruzzone <spam@unsigned.it>
2016-06-11 18:05:33 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('document').ready(function(){
+  $.get('cv.txt', function(data){
+    $("body > div.col-sm-6.left-column > pre")
+      .html(Prism.highlight(data, Prism.languages.json));
+  });
+});
