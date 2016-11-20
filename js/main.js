84e6c1895c8820f5c262ccbc9b0e875214880218
Luca Bruzzone <spam@unsigned.it>
2016-11-20 16:25:07 +0100

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $('document').ready(function(){
-  $.get('cv.txt', function(data){
+  $.get('cv.json', function(data){
     $("body > div.col-sm-6.left-column > pre")
       .html(Prism.highlight(data, Prism.languages.json));
   });
