e0090400bbdc1fba490e4f56cd3ef9b195f7f3d2
Luca Bruzzone <spam@unsigned.it>
2016-11-20 16:28:10 +0100

    fixes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $('document').ready(function(){
-  $.get('/cv', function(data){
+  $.get('./cv', function(data){
     $("body > div.col-sm-6.left-column > pre")
       .html(Prism.highlight(data, Prism.languages.json));
   });
